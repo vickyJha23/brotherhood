5ab5ecea66290c5b6bb51b61ac8f7fcd04d4a2e5
vickyjha23 <vickyjha388@gmail.com>
2025-02-04 00:27:49 +0530

    chore: Set up TypeScript, ESLint, Prettier, lint-staged, and Husky
    - Configured TypeScript for the project
    - Integrated ESLint with standard configurations for linting
    - Added Prettier for code formatting
    - Set up lint-staged for running ESLint and Prettier on staged files
    - Configured Husky for pre-commit hooks to enforce linting and formatting

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,20 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config({
+     extends: [
+         eslint.configs.recommended,
+         ...tseslint.configs.recommendedTypeChecked,
+         ...tseslint.configs.stylisticTypeChecked,
+         eslintConfigPrettier,
+     ],
+     languageOptions: {
+        parserOptions: {
+            projectService: true, // this is enables s
+            tsconfigRootDir: import.meta.dirname, 
+        },
+    },
+    files: ['**/*.ts'],   
+  }
+  );
