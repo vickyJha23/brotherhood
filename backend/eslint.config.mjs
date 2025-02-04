8e01918aa6673f08d87d4da2fb77c044044ab137
vickyjha23 <vickyjha388@gmail.com>
2025-02-04 17:06:41 +0530

    add:value

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,20 +1,19 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config({
-     extends: [
-         eslint.configs.recommended,
-         ...tseslint.configs.recommendedTypeChecked,
-         ...tseslint.configs.stylisticTypeChecked,
-         eslintConfigPrettier,
-     ],
-     languageOptions: {
-        parserOptions: {
-            projectService: true, // this is enables s
-            tsconfigRootDir: import.meta.dirname, 
-        },
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    eslintConfigPrettier,
+  ],
+  languageOptions: {
+    parserOptions: {
+      projectService: true, // this is enables s
+      tsconfigRootDir: import.meta.dirname,
     },
-    files: ['**/*.ts'],   
-  }
-  );
+  },
+  files: ['**/*.ts'],
+});
